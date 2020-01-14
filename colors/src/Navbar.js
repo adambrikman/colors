9f9e91b043e1db13df472d6fdd5b5ee38a373f43
Adam <adambrikman@gmail.com>
2020-01-13 22:52:20 -0800

    Add Navbar and Footer to SingleColorPalette

--- colors/src/Navbar.js
+++ colors/src/Navbar.js
@@ -37,7 +37,7 @@ export class Navbar extends Component {
 
     render() {
 
-        const {level, changeLevel} = this.props;
+        const {level, changeLevel, showingAllColors} = this.props;
         const {format} = this.state;
 
         return (
@@ -45,18 +45,22 @@ export class Navbar extends Component {
                 <div className="logo">
                     <Link to='/'>ReactColorPalette</Link>
                 </div>
-                <div className="slider-container">
-                    <span>Level: {level}</span>
-                    <div className="slider">
-                    <Slider 
-                        defaultValue={level}
-                        min={100} 
-                        max={900}  
-                        step={100}
-                        onAfterChange={changeLevel}
-                    />
+
+                {/* Display slider on non-single color palettes only */}
+                {showingAllColors && (
+                    <div className="slider-container">
+                        <span>Level: {level}</span>
+                        <div className="slider">
+                            <Slider 
+                                defaultValue={level}
+                                min={100} 
+                                max={900}  
+                                step={100}
+                                onAfterChange={changeLevel}
+                            />
+                        </div>
                     </div>
-                </div>
+                )}
                 <div className="select-container">
                     <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value='hex'>HEX - #ffffff</MenuItem>
--- colors/src/SingleColorPalette.js
+++ colors/src/SingleColorPalette.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
+import PaletteFooter from './PaletteFooter';
 import ColorBox from './ColorBox';
+import Palette from './Palette';
 
 export class SingleColorPalette extends Component {
 
     constructor(props) {
         super(props);
         this._shades = this.gatherShades(this.props.palette, this.props.colorId)
+        this.state = {
+            format: 'hex'
+        }
+        this.changeFormat = this.changeFormat.bind(this);
     }
 
     gatherShades(palette, colorToFilterBy) {
@@ -24,23 +31,29 @@ export class SingleColorPalette extends Component {
         return shades.slice(1);
     }
 
+    changeFormat(format) {
+        this.setState({ format })
+    }
+
     render() {
+
+        const {format} = this.state;
+        const {paletteName, emoji} = this.props.palette;
 
         const colorBoxes = this._shades.map(color => {
             return <ColorBox 
                 key={color.id}
                 name={color.name}
-                background={color.hex}
+                background={color[format]}
                 showLink={false}
             />
         });
 
         return (
             <div className='Palette'>
-                <h1>Single Color Palette</h1>
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
+                <Navbar handleChange={this.changeFormat} showingAllColors={false} />
+                <div className="Palette-colors">{colorBoxes}</div>
+                <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         )
     }
