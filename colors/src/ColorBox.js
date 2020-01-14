63bdb56d9c9ee8f955c42245ad53ea7f3147a0ef
Adam <adambrikman@gmail.com>
2020-01-13 20:49:00 -0800

    Add SingleColorPalette and Link

--- colors/src/ColorBox.js
+++ colors/src/ColorBox.js
@@ -21,7 +21,8 @@ class ColorBox extends Component {
     }
 
     render() {
-        const {name, background} = this.props;
+        const {name, background, paletteId, id} = this.props;
+        console.log(this.props);
         const {copied} = this.state;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -37,7 +38,7 @@ class ColorBox extends Component {
                         </div>
                         <button className="copy-button">Copy</button>
                     </div>
-                    <Link to="/" onClick={e => e.stopPropagation()}>
+                    <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
                         <span className="see-more">More</span>
                     </Link>
                 </div>
--- colors/src/Palette.js
+++ colors/src/Palette.js
@@ -29,11 +29,17 @@ class Palette extends Component {
     }
 
     render() {
-        const {colors, paletteName, emoji} = this.props.palette;
+        const {colors, paletteName, emoji, id} = this.props.palette;
         const {level, format} = this.state;
 
         const colorBoxes = colors[level].map(color => {
-            return <ColorBox key={color.id} background={color[format]} name={color.name} />
+            return <ColorBox 
+                key={color.id}
+                id={color.id}
+                background={color[format]} 
+                name={color.name}
+                paletteId={id}
+            />
         })
 
         return (
